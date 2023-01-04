df7effc27ac6a1f70a906973a98f69f6d4eb9614
contactatfp <85082344+contactatfp@users.noreply.github.com>
2023-01-04 10:46:59 -0800

    User Authentication and Hashed PW. Moved HTML location.

--- Flaskblog/flaskblog/__init__.py
+++ Flaskblog/flaskblog/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
@@ -8,5 +9,7 @@ db = SQLAlchemy(app)
 
 # app context needed for db.create_all()
 app.app_context().push()
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
+
--- Flaskblog/flaskblog/routes.py
+++ Flaskblog/flaskblog/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect
-from flaskblog import app
+from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
 
@@ -35,7 +35,11 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
+        hashed_pass = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_pass)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You can login', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
