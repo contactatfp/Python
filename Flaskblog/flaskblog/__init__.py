89398240dd0bbdb78ac98268bb3f340d708dd8d7
contactatfp <85082344+contactatfp@users.noreply.github.com>
2023-01-04 09:20:25 -0800

    added app context in init

--- Flaskblog/flaskblog/__init__.py
+++ Flaskblog/flaskblog/__init__.py
@@ -6,5 +6,7 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
+# app context needed for db.create_all()
+app.app_context().push()
 
 from flaskblog import routes
