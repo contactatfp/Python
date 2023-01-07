814db4328a9efb6df0b1787ed788c2fe21dae9e5
contactatfp <85082344+contactatfp@users.noreply.github.com>
2023-01-07 09:43:51 -0800

    Password Reset with Token. Sensitive IDs using environment variables

--- Flaskblog/flaskblog/__init__.py
+++ Flaskblog/flaskblog/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from os import environ
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
@@ -14,5 +16,15 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+
+app.config['MAIL_SERVER'] = 'smtp-relay.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = environ.get('EMAIL_PASS')
+mail = Mail(app)
+
+
 
 from flaskblog import routes
